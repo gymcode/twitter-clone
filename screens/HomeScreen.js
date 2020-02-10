a71059891b298f42630eb4b16daeb1b547c9f719
gymcode <kyleabs20@gmail.com>
2020-02-10 10:26:40 -0700

    design part of the ios settings screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,48 @@
-import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-
-import { MonoText } from '../components/StyledText';
+import {View, Text, Image} from 'react-native';
+import {Ionicons} from "@expo/vector-icons"
 
 const HomeScreen = ()=>{
   return(
-    <View style={{backgroundColor: 'blue', flex: 1, justifyContent: "center", alignItems: "center"}}>
-      <Text style={{fontSize: 25, color: '#fff'}}> Hello World </Text>
+    //the first one indicates that what ever is in it is javascript and the inner one indicate key-value pair
+    <View style={{height: 200, width:400, flex: 1,backgroundColor:"#e3e3e3"}}>
+
+      <View style={{paddingHorizontal: 20}}>
+        <Text style={{ fontSize: 24, fontWeight:"bold"}}>Settings</Text>
+      </View>
+
+      <View style={{backgroundColor: '#ffffff',  flexDirection: "row" }}>
+        <View style={{height: 100, flex: 2, alignItems: "center", justifyContent: "center"}}>
+          <Image source={require('../assets/images/avatar.jpg')} style={{width: 80, height:80, borderRadius: 50 }}/>
+        </View>
+        <View style={{  height: 100, flex: 5, justifyContent: "center",paddingHorizontal:10}}>
+            <Text style={{fontSize: 30}}>Kenneth Lartey</Text>
+            <Text>AppleID, iCloud, itunes & Appstore</Text>
+        </View>
+        <View style={{ height: 100, flex: 1, justifyContent: "center", alignItems: "center"}}>
+            <Ionicons
+              name="ios-arrow-forward"
+              size={26}
+            />
+        </View>  
+      </View>
+
+      <View style={{backgroundColor: "#ffffff", marginTop: 20, flexDirection: "row", height: 40 , borderWidth: 0.43, borderColor: '#dedede'}}>
+        <View style={{flex: 12, alignItems: "center", justifyContent: "space-between", flexDirection: "row"}}>
+          <Text style={{paddingHorizontal: 20}} >Finish setting up your Iphone</Text>
+          <View style={{backgroundColor: "red", width:20, height: 20, borderRadius: 10, alignItems: "center", justifyContent: "center"}}>
+            <Text style={{ color: '#ffffff', fontWeight: 'bold'}}>1</Text>
+          </View>
+        </View>
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
+              <Ionicons
+                name="ios-arrow-forward"
+                size={26}
+              />
+        </View>  
+      </View>
     </View>
   )
 }
 
-export default HomeScreen;
+export default HomeScreen
